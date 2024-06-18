a8135a8aee0c69b8c4944c3337a63f0324c312c5
ArtemKarDev <akm86@mail.ru>
2024-06-18 16:28:04 +0300

    added bibl owner for config

--- src/test/java/config/WebDriverProvider.java
+++ src/test/java/config/WebDriverProvider.java
@@ -2,15 +2,10 @@ package config;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.function.Supplier;
 import java.util.Map;
 
 public class WebDriverProvider {
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,7 +1,5 @@
 package tests;
 
-import com.codeborne.selenide.Config;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 
@@ -17,41 +15,17 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
-    //private WebDriver driver = new WebDriverProvider().get();
-
     @Step("Open site in browser")
     @BeforeAll
     static void setUpConfig() {
-
-        // убрал конфигурацию в webDriverProvider
-//        String browser = System.getProperty("browser", "chrome");
-//        String version = System.getProperty("webDriverVersion", "122.0");
-//        String windowSize = System.getProperty("browserWindowSize", "1920x1080");
-//        String wdHost = System.getProperty("wdHost", "selenoid.autotests.cloud");
-//        Configuration.remote = "https://user1:1234@" + wdHost + "/wd/hub";
-//        Configuration.browser = browser;
-//        Configuration.browserVersion = version;
-//        Configuration.browserSize = windowSize;
-//        Configuration.baseUrl = "https://demoqa.com";
 
         WebDriverProvider webDriverProvider = new WebDriverProvider();
         webDriverProvider.config();
 
         RestAssured.baseURI = "https://demoqa.com";
 
-        //пожалуйста, позвольте оставить эту строчку
-        //Configuration.holdBrowserOpen = true;
-
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("selenoid:options", Map.of(
-//                "enableVNC", true,
-//                "enableVideo", true
-//        ));
-//
-//        Configuration.browserCapabilities = capabilities;
-//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
-
 
     @Step("Attach video, screenshot and browser logs. Close browser. ")
     @AfterEach
